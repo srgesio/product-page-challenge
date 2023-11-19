f1bc1b5ffe3599c353cc1295b4dddd4173d5a63a
srgesio <gesioiajm@gmail.com>
2023-11-19 15:16:59 -0300

    feat: adding hover states on menu items and profile

--- app/components/Drawer/index.tsx
+++ app/components/Drawer/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MenuIcon from "../Icons/MenuIcon";
 import styles from './Drawer.module.css'
 import CloseIcon from "../Icons/CloseIcon";
@@ -7,16 +7,36 @@ import Navigation from "../Navigation";
 
 export default function Drawer() {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+    const modalRef = useRef<HTMLDivElement>(null)
     function toggleDrawer() {
         setIsDrawerOpen(!isDrawerOpen)
     }
+    const handleCloseModal = (event: MouseEvent) => {
+        if (modalRef.current && !modalRef.current.contains(event.target as Node) && isDrawerOpen) {
+            toggleDrawer()
+        }
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            handleCloseModal(event);
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [toggleDrawer])
+
+
     return (
         <>
             <div className={styles.menuTrigger} onClick={toggleDrawer}>
                 <MenuIcon />
             </div>
             <div className={`${styles.drawer} ${isDrawerOpen ? styles.open : ''}`}>
-                <div className={styles.drawerContent}>
+                <div className={styles.drawerContent} ref={modalRef}>
 
                     <div className={styles.menuTrigger} onClick={toggleDrawer}>
                         <CloseIcon />
